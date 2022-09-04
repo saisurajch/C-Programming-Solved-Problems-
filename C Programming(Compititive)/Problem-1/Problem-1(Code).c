b95b627fe9a6b3a4c5919ddf00f832606e0a744a
Sai Suraj <84668491+saisurajch@users.noreply.github.com>
2022-09-04 11:40:48 +0530

    Update Problem-1(Code).c

--- C Programming(Compititive)/Problem-1/Problem-1(Code).c
+++ C Programming(Compititive)/Problem-1/Problem-1(Code).c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d%d%d",&A,&B,&C);
     printf("Enter the topic given on contest day i.e X :");
     scanf("%d",&X);
-    //we are using nested if because the topics are only 10 topics
+    //we are using nested if because the topics are only 10
    if(A>0 && A<=10,B>0 && B<=10,C>0 && C<=10,X>0 && X<=10){
         //we are using OR operator
     if (X==A || X==B || X==C) {
